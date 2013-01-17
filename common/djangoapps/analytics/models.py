41a1aae211ac2da583820a18a39c91a528ee504c
jmvt <jm@edx.org>
2013-01-17 16:58:38 -0500

    Added migration for StudentGeoLocation model.

--- common/djangoapps/analytics/models.py
+++ common/djangoapps/analytics/models.py
@@ -17,6 +17,15 @@ class StudentGeoLocation(models.Model):
     
     We keep a one to one mapping user to geo location as the location can be considered unique for 
     each individual. 
+    
+    Migration Notes
+    
+    If you make changes to this model, be sure to create an appropriate migration
+    file and check it in at the same time as your model changes. To do that,
+    
+    1. Go to the mitx dir
+    2. django-admin.py schemamigration analytics --auto --settings=lms.envs.dev --pythonpath=. description_of_your_change
+    3. Add the migration file created in mitx/common/djangoapps/analytics/migrations/      
     """
 
     user = models.OneToOneField(User, unique=True, db_index=True)
