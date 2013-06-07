3349a170be8eac5521af305d7e1475cd60cb8b5a
Adam Palay <adam@edx.org>
2013-06-07 12:19:25 -0400

    deploy fixes

--- pavements/deploy.py
+++ pavements/deploy.py
@@ -9,12 +9,14 @@ from pavements.helpers import *
 # Packaging constants
 COMMIT = os.environ.get("GIT_COMMIT", os.system("git rev-parse HEAD")).strip()[0, 10]
 PACKAGE_NAME = "mitx"
-BRANCH = re.sub('origin/', '', (re.sub('refs/heads/', '', (ENV.get("GIT_BRANCH", `git symbolic-ref -q HEAD`).strip()))))
+BRANCH = re.sub('origin/', '', (re.sub('refs/heads/', '', (ENV.get("GIT_BRANCH", os.system("git symbolic-ref -q HEAD"))).strip())))
+
 
 @task
-def autoplay_properites():
-	with open("autodeploy.properties", "w") as f:
-		f.write("UPSTREAM_NOOP=false\n")
+def autodeploy_properties():
+    """Build a properties file used to trigger autodeploy builds"""
+    with open("autodeploy.properties", "w") as f:
+        f.write("UPSTREAM_NOOP=false\n")
         f.write("UPSTREAM_BRANCH=%s\n") % BRANCH
         f.write("UPSTREAM_JOB=%s\n") % PACKAGE_NAME
         f.write("UPSTREAM_REVISION=%s\n") % COMMIT
