049ca91e6d8f78a5fb81c9b942bf764b9eefbaeb
Will Daly <will@edx.org>
2013-08-01 09:23:39 -0400

    Moved CSRF override into its own settings file

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -10,6 +10,7 @@ Common traits:
 # We intentionally define lots of variables that aren't used, and
 # want to import all variables from base settings files
 # pylint: disable=W0401, W0614
+
 import json
 
 from .common import *
