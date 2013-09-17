2e2cea595c5a709d8552854106e1afea60542fcd
polesye <s2pak.anton@gmail.com>
2013-09-17 19:14:53 +0300

    Move js files from common to cms.

--- common/lib/xmodule/xmodule/video_module.py
+++ common/lib/xmodule/xmodule/video_module.py
@@ -147,14 +147,7 @@ class VideoModule(VideoFields, XModule):
             resource_string(__name__, 'js/src/video/07_video_volume_control.js'),
             resource_string(__name__, 'js/src/video/08_video_speed_control.js'),
             resource_string(__name__, 'js/src/video/09_video_caption.js'),
-            resource_string(__name__, 'js/src/video/10_main.js'),
-
-            resource_string(__name__, 'js/src/video/transcripts/transcripts_00.js'),
-            resource_string(__name__, 'js/src/video/transcripts/transcripts_01_utils.js'),
-            resource_string(__name__, 'js/src/video/transcripts/transcripts_02_editor.js'),
-            resource_string(__name__, 'js/src/video/transcripts/transcripts_03_metadata_videolist.js'),
-            resource_string(__name__, 'js/src/video/transcripts/transcripts_04_message_manager.js'),
-            resource_string(__name__, 'js/src/video/transcripts/transcripts_05_file_uploader.js')
+            resource_string(__name__, 'js/src/video/10_main.js')
         ]
     }
     css = {'scss': [resource_string(__name__, 'css/video/display.scss')]}
