76a6ebb6caaf16e9fc48f1d637781dbdd1003cc6
Julia Hansbrough <julia@edx.org>
2013-11-05 19:42:25 +0000

    Fixed some quality issues with previous commit

--- lms/djangoapps/verify_student/tasks.py
+++ lms/djangoapps/verify_student/tasks.py
@@ -1,12 +1,13 @@
 from djcelery import celery
 from verify_student.models import SoftwareSecurePhotoVerification
 
+
 @celery.task
 def retry_failed_photo_verifications():
-	"""
-	This method finds those PhotoVerifications with a status of
-	MUST_RETRY and attempts to verify them.
-	"""
-	attempts_to_retry = SoftwareSecurePhotoVerification.objects.filter(status='must_retry')
-	for attempt in attempts_to_retry:
-		attempt.submit()
+    """
+    This method finds those PhotoVerifications with a status of
+    MUST_RETRY and attempts to verify them.
+    """
+    attempts_to_retry = SoftwareSecurePhotoVerification.objects.filter(status='must_retry')
+    for attempt in attempts_to_retry:
+        attempt.submit()
--- lms/djangoapps/verify_student/tests/test_tasks.py
+++ lms/djangoapps/verify_student/tests/test_tasks.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
-from datetime import timedelta
 import json
 from nose.tools import (
-	assert_in, assert_is_none, assert_equals, assert_not_equals, assert_raises,
-	assert_true, assert_false
+    assert_equals, assert_true
 )
-from mock import MagicMock, patch
+from mock import patch
 from django.test import TestCase
 from django.conf import settings
 import requests
 import requests.exceptions
 
 from student.tests.factories import UserFactory
-from verify_student.models import SoftwareSecurePhotoVerification, VerificationException
+from verify_student.models import SoftwareSecurePhotoVerification
 from verify_student.tasks import retry_failed_photo_verifications
-from util.testing import UrlResetMixin
-import verify_student.models
 
 FAKE_SETTINGS = {
-	"SOFTWARE_SECURE" : {
-		"FACE_IMAGE_AES_KEY" : "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"API_ACCESS_KEY" : "BBBBBBBBBBBBBBBBBBBB",
-		"API_SECRET_KEY" : "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC",
-		"RSA_PUBLIC_KEY" : """-----BEGIN PUBLIC KEY-----
+    "SOFTWARE_SECURE": {
+        "FACE_IMAGE_AES_KEY": "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+        "API_ACCESS_KEY": "BBBBBBBBBBBBBBBBBBBB",
+        "API_SECRET_KEY": "CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC",
+        "RSA_PUBLIC_KEY": """-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAu2fUn20ZQtDpa1TKeCA/
 rDA2cEeFARjEr41AP6jqP/k3O7TeqFX6DgCBkxcjojRCs5IfE8TimBHtv/bcSx9o
 7PANTq/62ZLM9xAMpfCcU6aAd4+CVqQkXSYjj5TUqamzDFBkp67US8IPmw7I2Gaa
@@ -31,86 +27,88 @@ dyZCM9pBcvcH+60ma+nNg8GVGBAW/oLxILBtg+T3PuXSUvcu/r6lUFMHk55pU94d
 9A/T8ySJm379qU24ligMEetPk1o9CUasdaI96xfXVDyFhrzrntAmdD+HYCSPOQHz
 iwIDAQAB
 -----END PUBLIC KEY-----""",
-		"API_URL" : "http://localhost/verify_student/fake_endpoint",
-		"AWS_ACCESS_KEY" : "FAKEACCESSKEY",
-		"AWS_SECRET_KEY" : "FAKESECRETKEY",
-		"S3_BUCKET" : "fake-bucket"
-	}
+        "API_URL": "http://localhost/verify_student/fake_endpoint",
+        "AWS_ACCESS_KEY": "FAKEACCESSKEY",
+        "AWS_SECRET_KEY": "FAKESECRETKEY",
+        "S3_BUCKET": "fake-bucket"
+    }
 }
 
 
 class MockKey(object):
-	"""
-	Mocking a boto S3 Key object. It's a really dumb mock because once we
-	write data to S3, we never read it again. We simply generate a link to it
-	and pass that to Software Secure. Because of that, we don't even implement
-	the ability to pull back previously written content in this mock.
+    """
+    Mocking a boto S3 Key object. It's a really dumb mock because once we
+    write data to S3, we never read it again. We simply generate a link to it
+    and pass that to Software Secure. Because of that, we don't even implement
+    the ability to pull back previously written content in this mock.
 
-	Testing that the encryption/decryption roundtrip on the data works is in
-	test_ssencrypt.py
-	"""
-	def __init__(self, bucket):
-		self.bucket = bucket
+    Testing that the encryption/decryption roundtrip on the data works is in
+    test_ssencrypt.py
+    """
+    def __init__(self, bucket):
+        self.bucket = bucket
 
-	def set_contents_from_string(self, contents):
-		self.contents = contents
+    def set_contents_from_string(self, contents):
+        self.contents = contents
 
-	def generate_url(self, duration):
-		return "http://fake-edx-s3.edx.org/"
+    def generate_url(self, duration):  # TODO suppress pylint here
+        return "http://fake-edx-s3.edx.org/"
+
 
 class MockBucket(object):
-	"""Mocking a boto S3 Bucket object."""
-	def __init__(self, name):
-		self.name = name
+    """Mocking a boto S3 Bucket object."""
+    def __init__(self, name):
+        self.name = name
+
 
 class MockS3Connection(object):
-	"""Mocking a boto S3 Connection"""
-	def __init__(self, access_key, secret_key):
-		pass
+    """Mocking a boto S3 Connection"""
+    def __init__(self, access_key, secret_key):
+        pass
 
-	def get_bucket(self, bucket_name):
-		return MockBucket(bucket_name)
+    def get_bucket(self, bucket_name):
+        return MockBucket(bucket_name)
 
 def mock_software_secure_post(url, headers=None, data=None, **kwargs):
-	"""
-	Mocks our interface when we post to Software Secure. Does basic assertions
-	on the fields we send over to make sure we're not missing headers or giving
-	total garbage.
-	"""
-	data_dict = json.loads(data)
+    """
+    Mocks our interface when we post to Software Secure. Does basic assertions
+    on the fields we send over to make sure we're not missing headers or giving
+    total garbage.
+    """
+    data_dict = json.loads(data)
 
-	# Basic sanity checking on the keys
-	EXPECTED_KEYS = [
-		"EdX-ID", "ExpectedName", "PhotoID", "PhotoIDKey", "SendResponseTo",
-		"UserPhoto", "UserPhotoKey",
-	]
-	for key in EXPECTED_KEYS:
-		assert_true(
-			data_dict.get(key),
-			"'{}' must be present and not blank in JSON submitted to Software Secure".format(key)
-		)
+    # Basic sanity checking on the keys
+    EXPECTED_KEYS = [
+        "EdX-ID", "ExpectedName", "PhotoID", "PhotoIDKey", "SendResponseTo",
+        "UserPhoto", "UserPhotoKey",
+    ]
+    for key in EXPECTED_KEYS:
+        assert_true(
+            data_dict.get(key),
+            "'{}' must be present and not blank in JSON submitted to Software Secure".format(key)
+        )
 
-	# The keys should be stored as Base64 strings, i.e. this should not explode
-	photo_id_key = data_dict["PhotoIDKey"].decode("base64")
-	user_photo_key = data_dict["UserPhotoKey"].decode("base64")
+    # The keys should be stored as Base64 strings, i.e. this should not explode
+    data_dict["PhotoIDKey"].decode("base64")
+    data_dict["UserPhotoKey"].decode("base64")
 
-	response = requests.Response()
-	response.status_code = 200
+    response = requests.Response()
+    response.status_code = 200
 
-	return response
+    return response
 
 def mock_software_secure_post_error(url, headers=None, data=None, **kwargs):
-	"""
-	Simulates what happens if our post to Software Secure is rejected, for
-	whatever reason.
-	"""
-	response = requests.Response()
-	response.status_code = 400
-	return response
+    """
+    Simulates what happens if our post to Software Secure is rejected, for
+    whatever reason.
+    """
+    response = requests.Response()
+    response.status_code = 400
+    return response
 
 def mock_software_secure_post_unavailable(url, headers=None, data=None, **kwargs):
-	"""Simulates a connection failure when we try to submit to Software Secure."""
-	raise requests.exceptions.ConnectionError
+    """Simulates a connection failure when we try to submit to Software Secure."""
+    raise requests.exceptions.ConnectionError
 
 
 # Lots of patching to stub in our own settings, S3 substitutes, and HTTP posting
@@ -119,28 +117,38 @@ def mock_software_secure_post_unavailable(url, headers=None, data=None, **kwargs
 @patch('verify_student.models.Key', new=MockKey)
 @patch('verify_student.models.requests.post', new=mock_software_secure_post)
 class TestVerifyStudentTask(TestCase):
+    """
+    Tests for tasks in the verify_student module
+    """
 
-	def create_and_submit(self, username, error=None):
-		user = UserFactory.create()
-		attempt = SoftwareSecurePhotoVerification(user=user)
-		user.profile.name = username
-		attempt.upload_face_image("realdata")
-		attempt.upload_photo_id_image("yolo")
-		attempt.mark_ready()
-		attempt.submit()
-		return attempt
+    def create_and_submit(self, username):
+        """
+        Helper method that lets us create new SoftwareSecurePhotoVerifications
+        """
+        user = UserFactory.create()
+        attempt = SoftwareSecurePhotoVerification(user=user)
+        user.profile.name = username
+        attempt.upload_face_image("Fake Data")
+        attempt.upload_photo_id_image("More Fake Data")
+        attempt.mark_ready()
+        attempt.submit()
+        return attempt
 
-	def test_retry_failed_photo_verifications(self):
-		# set up some fake data to use...
-		self.create_and_submit("SuccessfulSally")
-		self.create_and_submit("SuccessfulSue")
-		with patch('verify_student.models.requests.post', new=mock_software_secure_post_error):
-			self.create_and_submit("RetryRoger")
-		with patch('verify_student.models.requests.post', new=mock_software_secure_post_error):
-			self.create_and_submit("RetryRick")
-		# check to make sure we had two successes and two failures; otherwise we've got problems elsewhere
-		assert_equals(len(SoftwareSecurePhotoVerification.objects.filter(status="submitted")), 2)
-		assert_equals(len(SoftwareSecurePhotoVerification.objects.filter(status='must_retry')), 2)
-		retry_failed_photo_verifications()
-		attempts_to_retry = SoftwareSecurePhotoVerification.objects.filter(status='must_retry')
-		assert_equals(bool(attempts_to_retry), False)
+    def test_retry_failed_photo_verifications(self):
+        """
+        Tests that the task used to find "must_retry" SoftwareSecurePhotoVerifications
+        and re-submit them executes successfully
+        """
+        # set up some fake data to use...
+        self.create_and_submit("SuccessfulSally")
+        self.create_and_submit("SuccessfulSue")
+        with patch('verify_student.models.requests.post', new=mock_software_secure_post_error):
+            self.create_and_submit("RetryRoger")
+        with patch('verify_student.models.requests.post', new=mock_software_secure_post_error):
+            self.create_and_submit("RetryRick")
+        # check to make sure we had two successes and two failures; otherwise we've got problems elsewhere
+        assert_equals(len(SoftwareSecurePhotoVerification.objects.filter(status="submitted")), 2)
+        assert_equals(len(SoftwareSecurePhotoVerification.objects.filter(status='must_retry')), 2)
+        retry_failed_photo_verifications()
+        attempts_to_retry = SoftwareSecurePhotoVerification.objects.filter(status='must_retry')
+        assert_equals(bool(attempts_to_retry), False)
